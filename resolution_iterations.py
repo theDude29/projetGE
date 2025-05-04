6fb317efd2fb3ea1f2786d82420e8589bb478c48
theDude29 <remi.perenne@free.fr>
2025-05-04 09:46:45 +0200

    correction de qlq applications numériques

--- resolution_iterations.py
+++ resolution_iterations.py
@@ -5,15 +5,18 @@ avec A,B,C et T1 connues.
 On utilise une methode de linéarisation.
 '''
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 def nouveauT2(A,B,C,T1,T2):
 	D = 4*B*((T1+T2)/2)**3
 	return (T1*(D+C) - A)/(D+C)
 
-A = -41.3*10**6
-B = 1.04*10**(-5)
+A = -84.59*10**6
+B = -1.04*10**(-5)
 C = 3649.54
 T1 = 293
-precision = 0.1
+precision = 100000
 
 T2 = 3000
 prochainT2 = nouveauT2(A,B,C,T1,T2)
@@ -26,6 +29,14 @@ while abs(T2 - prochainT2) > precision:
 	T2 = prochainT2
 	prochainT2 = nouveauT2(A,B,C,T1,T2)
 	i+=1
+	
+def f(x):
+	return A - B*(T1**4-x**4) - C*(T1-x)
+
+X = np.arange(0,1000,10)
+Y = f(X)
+plt.plot(X,Y)
+plt.show();
 
 print("solution trouvée:", T2)
 print("verification:",A - B*(T1**4-T2**4) - C*(T1-T2))
